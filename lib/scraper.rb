e3c5d874ff2e4d175f0d9af89ed37a0549aeb3f7
Sierra Wallace <sskwallace@gmail.com>
2020-11-05 00:30:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,20 @@ end
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     student = {} 
-    
+    container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+    container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+      student[:profile_quote] = page.css(".profile-quote").text
+      student[:bio] = page.css("div.description-holder p").text
+      student
   end
 end
