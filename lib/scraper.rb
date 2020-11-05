edf9f740f8fcce89081313e15cab1865c62f1183
Sierra Wallace <sskwallace@gmail.com>
2020-11-05 00:17:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,17 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = [] 
-    
+    doc.css("div.student-card").each do 
+      |student|
+      name = student.css("student-name").text, 
+      location = student.css("student-location").text, 
+      profile_url = student.css("a").attr("href").value 
+      student_info = {:name => name, :location => location, :profile_url => profile_url}
+      students << student_info
+    end
+    students
+  end
+end
   def self.scrape_profile_page(profile_url)
     
   end
