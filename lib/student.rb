b636e999f6282b83060a1a877319500f7e3c1870
Sierra Wallace <sskwallace@gmail.com>
2020-11-05 00:35:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      self.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
