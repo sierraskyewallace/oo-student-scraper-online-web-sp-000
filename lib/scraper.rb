a988d3910bb5107b9fe42adb188bc87402bed8b9
Sierra Wallace <sskwallace@gmail.com>
2020-11-05 00:02:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,14 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
+    students = [] 
     
-  end
-
   def self.scrape_profile_page(profile_url)
     
   end
-
-end
 
