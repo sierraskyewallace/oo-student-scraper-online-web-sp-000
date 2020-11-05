eea595b466c614068320e96f54f9393fcfd19c84
Sierra Wallace <sskwallace@gmail.com>
2020-11-05 00:38:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,39 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
-  end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
+    doc = Nokogiri::HTML(open(index_url))
+    students = [] 
+    doc.css("div.student-card").each do 
+      |student|
+      name = student.css(".student-name").text
+      location = student.css(".student-location").text 
+      profile_url = student.css("a").attr("href").value 
+      student_info = {:name => name, :location => location, :profile_url => profile_url}
+      students << student_info
+    end
+    students
 end
-
+  def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {} 
+    container = doc.css(".social-icon-container a").map{|icon| icon.attribute("href").value}
+    container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+      student[:profile_quote] = doc.css(".profile-quote").text
+      student[:bio] = doc.css("div.description-holder p").text
+      student
+  end
+end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,24 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
-  end
-
-  def add_student_attributes(attributes_hash)
-    
-  end
-
-  def self.all
-    
+    students_array.each do |student_hash|
+      self.new(student_hash)
   end
 end
 
+  def add_student_attributes(attributes_hash)
+    attributes_hash.each do |key, value|
+      self.send(("#{key}="), value)
+  end
+  self
+end
+
+  def self.all
+    @@all 
+  end
+end
